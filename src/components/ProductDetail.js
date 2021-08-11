8b9dcf96a2f0b689d5aa7ca4a08e505316fb5a89
Thiago <thiagoc.carboneri@gmail.com>
2021-08-11 15:51:59 -0300

    continuando requisito

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,12 +1,26 @@
 import React from 'react';
-// import * as api from '../services/api';
+import * as api from '../services/api';
 
 class ProductDetails extends React.Component {
   render() {
     return (
-      <h1>
-        Olá
-      </h1>
+      <div className="product-detail">
+        <h1
+          data-testid="product-detail-name"
+        >
+          { name }
+        </h1>
+        <img src={ thumbnail } alt={ name } />
+        <h2>
+          { available_quantity }
+        </h2>
+        <h2>
+          { price }
+        </h2>
+        <h2>
+          { accepts_mercadopago && <h2> Aceita Mercado Pago! </h2>}
+        </h2>
+      </div>
     );
   }
 }
